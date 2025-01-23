4c2e82520d401a940607347a353957b26f12b10c
Lorraine Scanlon <lorrainescanlon@gmail.com>
2025-01-23 12:45:24 +0000

    update secret key locally and on heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#o7uskv#xe@8-%^u9f&jyyh9peh6xd5^5)3$rvp#l-zl1d(*y('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
