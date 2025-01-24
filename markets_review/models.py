297ffae4c047418b83216dbf5f6a8678fc8f328b
Lorraine Scanlon <lorrainescanlon@gmail.com>
2025-01-24 23:52:14 +0000

    updated market model to include location and time fields, added model to admin.py

--- markets_review/models.py
+++ markets_review/models.py
@@ -5,11 +5,13 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class market(models.Model):
+class Market(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey( User, on_delete=models.CASCADE, related_name="marekt_posts")
     image = CloudinaryField('image', default='placeholder')
+    location = models.CharField(default = 'unknown')
+    day_time = models.CharField(default='unknown')
     latitude = models.FloatField()
     longitude = models.FloatField()
     content = models.TextField()
--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lorrainesca-farmersmark-3hinj5lt646.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-lorrainesca-farmersmark-3hinj5lt646.ws.codeinstitute-ide.net', '.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -78,6 +78,11 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+CSRF_TRUSTED_ORIGINS = {
+    "https://*.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
 }
 
 
