a55dfc08fe75176229050b7c9e59bbd60d4a0f22
Lorraine Scanlon <lorrainescanlon@gmail.com>
2025-01-26 00:43:34 +0000

    installed summernote package to provide additional functionality to admin dashboard

--- markets_review/admin.py
+++ markets_review/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Market, Review, Ratings
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Market)
+class MarketAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'status')
+    search_fields = ['name']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Market)
 admin.site.register(Review)
 admin.site.register(Ratings)
