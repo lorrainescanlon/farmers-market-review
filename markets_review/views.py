cced80e4bb711e6ac22cc5a210863108dbde06ec
Lorraine Scanlon <lorrainescanlon@gmail.com>
2025-01-27 23:09:35 +0000

    updated MarketList view to order objects by name, updated market images

--- markets_review/views.py
+++ markets_review/views.py
@@ -5,6 +5,6 @@ from .models import Market
 # Create your views here.
 
 class MarketList(generic.ListView):
-    queryset = Market.objects.all()
+    queryset = Market.objects.all().order_by('name')
     template_name = "markets_review/index.html"
     paginate_by = 6
