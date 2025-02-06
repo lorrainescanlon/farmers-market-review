b8fff4d2bc0c80c289e357da9ae86256777fd5da
lorrainescanlon <lorrainescanlon@gmail.com>
2025-02-06 23:46:59 +0000

    updated review form to include labels for fields, used css class to hide asterisks for required fields, moved form to the right of the displayed reviews

--- markets_review/forms.py
+++ markets_review/forms.py
@@ -5,4 +5,9 @@ from django import forms
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ('body', 'stars_rating', 'visit_again', )
+        fields = ['body', 'stars_rating', 'visit_again']
+        labels = {
+            'body': 'Your comments',
+            'stars_rating': 'Rate your exprience from 0 to 5',
+            'visit_again': 'Would you visit again?'
+        }
