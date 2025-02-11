b2d48ade49b686a3ee8cfb5df9e7102b2b5e29c4
lorrainescanlon <lorrainescanlon@gmail.com>
2025-02-11 21:43:32 +0000

    updated contact views.py to handle post request from user, contact message now showing in admin panel

--- contact/views.py
+++ contact/views.py
@@ -10,6 +10,12 @@ def contact(request):
     """
     Renders the Contact page
     """
+    if request.method =="POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, "Contact form recieved! We appreciate your feedback & will be in contact shortly.")
+
     contact_form = ContactForm()
     context = {
         "contact_form": contact_form, 
