9160a4117c3156093903ae9c4d50c363896c3fc7
lorrainescanlon <lorrainescanlon@gmail.com>
2025-03-04 23:42:02 +0000

    created news model, updated news url, view and admin.py files. Update css to centre market column column on mobile devices

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import News
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(News)
+class News(admin.ModelAdmin):
+
+    list_display = ('title', 'status', 'created_on', 'author', )
+    search_fields = ['title', 'author',]
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}  
+    summernote_fields = ('content',)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+import cloudinary
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# News Model.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_article"
+    )
+    content = models.TextField()
+    excerpt = models.TextField(blank=False)
+    image = CloudinaryField('image', null=True, default=None, blank=True)
+    created_on = models.DateField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
